906dc45d50ea21aa56cda8aa134f2c6078407dcc
kobi2294@yahoo.com <kobi2294@yahoo.com>
2021-05-02 16:29:16 +0300

    Fun with delegates

---
+++ Day 01/FunWithDelegates/FunWithDelegates/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace FunWithDelegates
+{
+    delegate void FunctionWithoutParams();
+    delegate void FunctionWithSingleIntParam(int a);
+
+    delegate int SingleNumberParamWithIntResult(int a);
+
+    class Program
+    {
+        private static int _functionThatMultipliesByTwo(int a)
+        {
+            return a * 2;
+        }
+
+        static void Main(string[] args)
+        {
+            var something = new Something();
+
+            FunctionWithSingleIntParam f1 = something.Increase;
+            f1 = something.Decrease;
+
+            f1(12);
+
+            int b = 20;
+
+            // .net 2
+            SingleNumberParamWithIntResult funckyFunc = delegate (int a)
+            {
+                return a * b;
+            };
+
+            // .net 3 with lambda expressions
+            SingleNumberParamWithIntResult funckyFunc2 = a => a * b;
+
+
+            // .net 3.5 + 
+            Func<int, int> f3 = a => a * 2;
+
+            Func<string, int, int, bool> f4 = (str, num1, num2) => str.Length > num1 + num2;
+            Action a = () => Console.WriteLine("Hello World");
+            Action<int> a2 = (int i) => Console.WriteLine(i);
+            Action<string, bool, int, Something> a3 = (str, b, i, s) => { };
+
+
+            MetodatEzer();
+
+            f1.Invoke(12);
+            Console.WriteLine(funckyFunc(15));
+        }
+
+        public static void MetodatEzer()
+        {
+            SingleNumberParamWithIntResult funckyFunc = delegate (int a)
+            {
+                return a * 2;
+            };
+
+        }
+    }
+}
---
+++ Day 01/FunWithDelegates/FunWithDelegates/Something.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunWithDelegates
+{
+    public class Something
+    {
+        int _a;
+
+        public Something()
+        {
+            _a = 5;
+        }
+
+        public void Increase(int delta)
+        {
+            _a += delta;
+        }
+
+        public void Decrease(int delta)
+        {
+            _a -= delta;
+        }
+    }
+}
