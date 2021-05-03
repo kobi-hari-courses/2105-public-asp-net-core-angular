9aebbcc75737db2bcbc4d283468f849f97ce9c3d
Kobi Hari <kobi2294@yahoo.com>
2021-05-04 00:05:27 +0300

    Homework 1 Solution

---
+++ Day 02/Homework1Solution/Homework1Solution/EnumerableHelper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1Solution
+{
+    public static class EnumerableHelper
+    {
+        // returns the items in source, except for the first 'count' items
+        public static IEnumerable<T> Skip<T>(this IEnumerable<T> source, int count)
+        {
+            var counter = 0;
+            foreach (var item in source)
+            {
+                counter++;
+                if (counter > count)
+                    yield return item;
+            }
+        }
+
+        // returns the items in source, except all the first items that do not match a condition
+        // once one item is found that matches the condition, the rest of the items are returned regardless of the condition
+        public static IEnumerable<T> SkipUntil<T>(this IEnumerable<T> source, Func<T, bool> condition)
+        {
+            var foundTrueItem = false;
+
+            foreach (var item in source)
+            {
+                foundTrueItem = foundTrueItem | condition(item);
+                if (foundTrueItem) yield return item;
+            }
+        }
+
+        // takes 2 enumerables as source, and returns one of each alternating between them until one of them is completed
+        public static IEnumerable<T> Alternate<T>(this IEnumerable<T> source, IEnumerable<T> source2)
+        {
+            // note that since we need 2 enumerators to co-exist at the same time, we cannot use `foreach` and need to create 
+            // the enumerators explicitly
+
+            using (var enumerator1 = source.GetEnumerator())
+            using (var enumerator2 = source2.GetEnumerator())
+            {
+                while (true)
+                {
+                    if (enumerator1.MoveNext())
+                    {
+                        yield return enumerator1.Current;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    if (enumerator2.MoveNext())
+                    {
+                        yield return enumerator2.Current;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public static bool Assert<T>(this IEnumerable<T> source, params T[] expected)
+        {
+            using (var enumerator1 = source.GetEnumerator())
+            using (var enumerator2 = expected.ToList().GetEnumerator())
+            {
+                while(true)
+                {
+                    var hasItem1 = enumerator1.MoveNext();
+                    var hasItem2 = enumerator2.MoveNext();
+
+                    // if both enumerabes ended at the same time, they are equal
+                    if (!hasItem1 && !hasItem2) return true;
+
+                    // if one of them ended and one did not, the are not equals
+                    if (!hasItem1 || !hasItem2) return false;
+
+                    // if we got here, both enumerators are still pointing to a current item
+                    // if the current items are different, they are not equal
+                    if (!Equals(enumerator1.Current, enumerator2.Current)) return false;
+
+                    // otherwise, move on to the next item
+                }
+            }
+        }
+    }
+}
---
+++ Day 02/Homework1Solution/Homework1Solution/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Homework1Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSkip();
+            TestSkipUntil();
+            TestAlternate();
+        }
+
+        public static void TestSkip()
+        {
+            var list = new List<int> { 1, 3, 5, 7, 9, 11 };
+
+            var result = list.Skip(4);
+            var success = result.Assert(9, 11);
+            Console.WriteLine($"Skip test result: { success}");
+        }
+
+        public static void TestSkipUntil()
+        {
+            var list = new List<int> { 1, 6, 10, 4, 9, 12 };
+            
+            var result = list.SkipUntil(i => i > 9);
+            var success = result.Assert(10, 4, 9, 12);
+
+            Console.WriteLine($"SkipUntil test result: {success}");
+        }
+
+        public static void TestAlternate()
+        {
+            var list1 = new List<int> { 1, 2, 3, 4, 5 };
+            var list2 = new List<int> { 10, 20, 30 };
+
+            var result = list1.Alternate(list2);
+            var success = result.Assert(1, 10, 2, 20, 3, 30, 4);
+
+            Console.WriteLine($"Alternate test result: {success}");
+        }
+    }
+}
