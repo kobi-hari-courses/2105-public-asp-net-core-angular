2296f2d1b9ddfbc87e301edd4813129e80b311e0
kobi2294@yahoo.com <kobi2294@yahoo.com>
2021-05-03 16:42:54 +0300

    Fun before LINQ

---
+++ Day 02/FunBeforeLinq/FunBeforeLinq/EnumerableHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Text;
+
+namespace FunBeforeLinq
+{
+    public static class EnumerableHelper
+    {
+        public static IEnumerable<int> RangeTo(this int start, int finish)
+        {
+            var current = start;
+            while (current <= finish)
+            {
+                yield return current;
+                current++;
+            }
+        }
+
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> condition)
+        {
+            foreach (var item in source)
+            {
+                if (condition(item))
+                    yield return item;
+            }
+        }
+
+        public static IEnumerable<TRes> Map<TSrc, TRes>(this IEnumerable<TSrc> source, Func<TSrc, TRes> projection)
+        {
+            foreach (var item in source)
+            {
+                yield return projection(item);
+            }
+        }
+    }
+}
---
+++ Day 02/FunBeforeLinq/FunBeforeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FunBeforeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in 2.RangeTo(50)
+                .Filter(i => i > 5)
+                .Filter(i => i % 3 == 0)
+                .Filter(i => i % 10 == 5)
+                .Map(i => -i)
+                )
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
