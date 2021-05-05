627a13a560c0e9f417701063b71c89e297c7af15
kobi2294@yahoo.com <kobi2294@yahoo.com>
2021-05-05 16:50:50 +0300

    TPL part 1

---
+++ Day 04/FunWithTpl/TplDemo/Helper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TplDemo
+{
+    public static class Helper
+    {
+        public static bool IsPrime(this int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        public static IEnumerable<int> GetAllPrimesUpTo(int number)
+        {
+            return Enumerable.Range(1, number)
+                .Where(i => i.IsPrime());
+        }
+
+        public static IEnumerable<int> GetAllPrimesParallelUpTo(int number)
+        {
+            return Enumerable.Range(1, number)
+                .AsParallel()
+                .Where(i => i.IsPrime());
+        }
+
+        public static Task<List<int>> GetAllPrimesAsync(int number)
+        {
+            return Task.Factory.StartNew(() => GetAllPrimesUpTo(number).ToList());
+        }
+
+    }
+}
