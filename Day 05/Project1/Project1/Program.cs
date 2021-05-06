158a77bf2f7fe6570b594ba4bf7168b875f762f0
Kobi Hari <kobi2294@yahoo.com>
2021-05-06 14:45:22 +0300

    Project 1 Completed

--- Day 05/Project1/Project1/Program.cs
+++ Day 05/Project1/Project1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Project1
@@ -48,6 +49,30 @@ namespace Project1
             }
         }
 
+        public static string GetUserSelectionFromStrings(IEnumerable<string> options)
+        {
+            var all = options
+                .OrderBy(m => m)
+                .Select((m, index) => (name: m, index: index))
+                .ToList();
+
+            Func<int> menu = () =>
+            {
+                Print("Please select: ", ConsoleColor.Green);
+                foreach (var item in all)
+                {
+                    Print($"{item.index + 1} {item.name}", ConsoleColor.Yellow);
+                }
+
+                return all.Count;
+            };
+
+            var option = GetUserSelectionFromMenu(menu);
+            var selected = all[option - 1].name;
+
+            return selected;
+        }
+
         public static void ListAllManufacturers()
         {
             var all = DataReader
@@ -66,21 +91,9 @@ namespace Project1
             var manufacturers = DataReader
                 .GetAllManufacturers()
                 .OrderBy(m => m.Name)
-                .Select((m, index) => (name: m.Name, index: index))
-                .ToList();
+                .Select(m => m.Name);
 
-            Func<int> menu = () =>
-            {
-                Print("Please select a manufacturer: ", ConsoleColor.Green);
-                foreach (var item in manufacturers)
-                {
-                    Print($"{item.index + 1} {item.name}", ConsoleColor.Yellow);
-                }
-                return manufacturers.Count;
-            };
-
-            var option = GetUserSelectionFromMenu(menu);
-            var selected = manufacturers[option - 1].name;
+            var selected = GetUserSelectionFromStrings(manufacturers);
 
             var cars = DataReader.GetAllCars()
                 .Where(car => car.Make == selected)
@@ -104,16 +117,70 @@ namespace Project1
             {
                 Print($"{item.name,-35} {item.count}", ConsoleColor.Cyan);
             }
-                
-
         }
 
         public static void ListBestCarsOfManufacturers()
         {
+            var countries = DataReader
+                .GetAllManufacturers()
+                .Select(m => m.Country)
+                .Distinct();
+
+            var selected = GetUserSelectionFromStrings(countries);
+
+            var manufactuers = DataReader
+                .GetAllManufacturers()
+                .Where(m => m.Country == selected)
+                .Select(m => m.Name)
+                .ToHashSet();
+
+            var carGroups = DataReader
+                .GetAllCars()
+                .Where(m => manufactuers.Contains(m.Make))
+                .GroupBy(m => m.Make)
+                .Select(group => (
+                        make: group.Key,
+                        cars: group.OrderByDescending(car => car.CombinedFe)
+                                   .Take(3)));
+
+            foreach (var group in carGroups)
+            {
+                Print(group.make, ConsoleColor.Cyan);
+                foreach (var car in group.cars)
+                {
+                    Print($"{car.Model,-30} {car.Year,-5} {car.CombinedFe}", ConsoleColor.White);
+                }
+            }
+
         }
 
         public static void ListGeneralHighlights()
         {
+            var cars = DataReader.GetAllCars().ToList();
+            var mfgs = DataReader.GetAllManufacturers().ToList();
+
+            var carWithLowestConbinedFe = cars
+                .OrderBy(c => c.CombinedFe)
+                .First();
+
+            var avgCityFe = cars.Average(c => c.CityFe);
+
+            var numberOfCounters = mfgs
+                .Select(m => m.Country)
+                .Distinct()
+                .Count();
+
+            var mfgWithHighestAvarageCombinedFe = cars
+                .GroupBy(car => car.Make)
+                .Select(group => (make: group.Key, avg: group.Average(car => car.CombinedFe)))
+                .OrderByDescending(pair => pair.avg)
+                .First();
+
+            Print($"Car with lowest combined FE: {carWithLowestConbinedFe.Make} {carWithLowestConbinedFe.Model}: {carWithLowestConbinedFe.CombinedFe}", ConsoleColor.White);
+            Print($"Average City FE: {avgCityFe}", ConsoleColor.White);
+            Print($"Number of different countries: {numberOfCounters}", ConsoleColor.White);
+            Print($"Manufacturer with highest average combined FE: {mfgWithHighestAvarageCombinedFe.make} ({mfgWithHighestAvarageCombinedFe.avg})", ConsoleColor.White);
+
         }
 
 
