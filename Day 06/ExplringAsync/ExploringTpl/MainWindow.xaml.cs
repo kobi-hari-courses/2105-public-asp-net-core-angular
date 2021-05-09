4c48709b951e4b8d71f76e464313896e5860e3de
kobi2294@yahoo.com <kobi2294@yahoo.com>
2021-05-09 12:09:49 +0300

    Finished TPL

---
+++ Day 06/ExplringAsync/ExploringTpl/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExploringTpl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await CalculateStockUsingAllOfThem();
+            MessageBox.Show("Finished");
+        }
+
+        public async Task Go()
+        {
+            Debug.WriteLine("1");
+            var t = Task.Delay(2000);
+            Debug.WriteLine("2");
+
+            var theAnswer = GetTheAnswerOfLifeTheUniverseAndEverything();
+
+            await t;
+            Debug.WriteLine("3");
+
+        }
+
+        public Task SomethingThatEndsWhenTheUserClicksAnotherButton()
+        {
+            stopButton.Visibility = Visibility.Visible;
+            var tcs = new TaskCompletionSource<int>();
+
+
+            stopButton.Click += (s, e) =>
+            {
+                tcs.TrySetResult(50);
+            };
+
+            return tcs.Task;
+        }
+
+        public Task SomethingThatTakesAWhile()
+        {
+            return Task.Delay(4000);
+        }
+
+        public void OldGo()
+        {
+            Debug.WriteLine("1");
+            var t = Task.Delay(2000);
+            Debug.WriteLine("2");
+            var c = t.ContinueWith(_ =>
+            {
+                Debug.WriteLine("3");
+                return true;
+            });
+        }
+
+        public Task<int> GetTheAnswerOfLifeTheUniverseAndEverything()
+        {
+            return Task.FromResult(42);
+        }
+
+        public Task<int> SomethingWentWrongAsync()
+        {
+            return Task.FromException<int>(new NullReferenceException());
+        }
+
+        public async Task<int> CalcualteStockIDontCareHow()
+        {
+            var someTasks = new List<Task<int>>
+            {
+                CalculateStockUsingGoogle(),   // 4 sec
+                CalculateStockUsingYahoo(),    // 2 sec
+                CalculateStockUsingAltaVista(),// 3 sec
+            };
+
+            var tRace = Task.WhenAny(someTasks);
+
+            var tWinner = await tRace;
+
+            var losers = someTasks.Except(new[] { tWinner });
+
+            return await tWinner;
+        }
+
+        public async Task<int> CalculateStockUsingAllOfThem()
+        {
+            var someTasks = new List<Task<int>>
+            {
+                CalculateStockUsingGoogle(),   // 4 sec
+                CalculateStockUsingYahoo(),    // 2 sec
+                CalculateStockUsingAltaVista(),// 3 sec
+            };
+
+            var all = await Task.WhenAll(someTasks);
+
+            return all.Sum();
+        }
+
+        public async Task<int> CalculateStockUsingGoogle()
+        {
+            var rnd = new Random();
+            var delay = rnd.Next(1000, 3000);
+
+            await Task.Delay(delay);
+            return 42;
+        }
+
+        public async Task<int> CalculateStockUsingYahoo()
+        {
+            var rnd = new Random();
+            var delay = rnd.Next(1000, 3000);
+
+            await Task.Delay(delay);
+            return 42;
+        }
+
+        public async Task<int> CalculateStockUsingAltaVista()
+        {
+            var rnd = new Random();
+            var delay = rnd.Next(1000, 3000);
+
+            await Task.Delay(delay);
+            return 42;
+        }
+
+
+    }
+}
---
+++ Day 06/FunWithTpl/TplDemo/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TplDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            txtPleaseWait.Visibility = Visibility.Visible;
+            progressBar.IsIndeterminate = true;
+            btnGo.IsEnabled = false;
+
+            var task = Helper.GetAllPrimesAsync(160000);
+
+            var res = await task;
+
+            lstResults.ItemsSource = task.Result;
+
+            btnGo.IsEnabled = true;
+            progressBar.IsIndeterminate = false;
+            txtPleaseWait.Visibility = Visibility.Collapsed;
+
+
+            //task.ContinueWith(t =>
+            //{
+            //    lstResults.ItemsSource = task.Result;
+
+            //    btnGo.IsEnabled = true;
+            //    progressBar.IsIndeterminate = false;
+            //    txtPleaseWait.Visibility = Visibility.Collapsed;
+
+            //}, TaskScheduler.FromCurrentSynchronizationContext());
+
+            //Console.WriteLine("Hello");
+
+
+        }
+    }
+}
