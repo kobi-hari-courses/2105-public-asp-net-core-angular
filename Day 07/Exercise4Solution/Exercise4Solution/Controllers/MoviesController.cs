161554cda58ad2372b6728c6b36c933dbb2e358d
kobi2294@yahoo.com <kobi2294@yahoo.com>
2021-05-10 16:11:44 +0300

    exercise 4 - solution completed

---
+++ Day 07/Exercise4Solution/Exercise4Solution/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise4Solution.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<string> _movies = new List<string>
+        {
+            "Reservoir Dogs",
+            "Pulp Fiction",
+            "Kill Bill 1",
+            "Kill Bill 2",
+            "Inglorious Bastards",
+            "Django Unleashed"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> GetAll([FromQuery] string startsWith)
+        {
+            var prefix = startsWith ?? "";
+            return _movies.Where(m => m.StartsWith(prefix));
+        }
+
+        [HttpPost]
+        public void Add([FromBody] string movie)
+        {
+            _movies.Add(movie);
+        }
+
+        [HttpPut("{index}")]
+        public void Replace(int index, [FromBody] string movie)
+        {
+            _movies[index] = movie;
+        }
+
+        [HttpDelete("{index}")]
+        public void Delete(int index)
+        {
+            _movies.RemoveAt(index);
+        }
+
+    }
+}
