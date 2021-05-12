b8af5ba1c24d4e40976f6508c0d4d9436e64f52a
Kobi Hari <kobi2294@yahoo.com>
2021-05-12 17:45:49 +0300

    Step 4: Cars controller

---
+++ Day 09/Project2Solution/Project2Solution/Controllers/Cars.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project2Solution.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2Solution.Controllers
+{
+    [Route("api/cars")]
+    [ApiController]
+    public class Cars : ControllerBase
+    {
+        private IRepositoryService _repo;
+
+        public Cars(IRepositoryService repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cars>>> GetAllCars()
+        {
+            var all = await _repo.GetAllCars();
+            return Ok(all);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cars>> GetCar(Guid id)
+        {
+            try
+            {
+                var car = await _repo.GetCarById(id);
+                return Ok(car);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
