dba7bbda0f204b4e85a8659070c414b4eb7f836f
Kobi Hari <kobi2294@yahoo.com>
2021-05-12 18:52:10 +0300

    Added swagger support

--- Day 09/Project2Solution/Project2Solution/Startup.cs
+++ Day 09/Project2Solution/Project2Solution/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Project2Solution.Middleware;
 using Project2Solution.Services;
 using System;
@@ -30,6 +31,11 @@ namespace Project2Solution
             services.AddTransient<IDataReaderService, DataReaderService>();
             services.AddSingleton<IRepositoryService, RepositoryService>();
             services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cars and Manufacturers", Version = "v1" });
+        });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +45,9 @@ namespace Project2Solution
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Project2Solution v1"));
 
             app.UseRouting();
 
