2cb4fb68caab22ce4bb651e5fff5239eef687f86
Kobi Hari <kobi2294@yahoo.com>
2021-05-12 09:51:32 +0300

    Day 9 - Courses app, initial revision with entities

---
+++ Day 09/CoursesApp/CoursesApp/Model/Entities/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursesApp.Model.Entities
+{
+    public record Course(
+        int Id, 
+        string Name, 
+        string Category, 
+        int InstructorId
+        );
+}
---
+++ Day 09/CoursesApp/CoursesApp/Model/Entities/Instructor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursesApp.Model.Entities
+{
+    public record Instructor(
+        int Id, 
+        string Name
+        );
+}
