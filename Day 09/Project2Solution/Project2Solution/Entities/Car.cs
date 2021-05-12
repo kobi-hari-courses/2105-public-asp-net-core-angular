4680e68f95c2c7a3f09978c96d46fb14e37438bb
Kobi Hari <kobi2294@yahoo.com>
2021-05-12 15:56:37 +0300

    Project 2 - Step 1 completed

---
+++ Day 09/Project2Solution/Project2Solution/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2Solution.Entities
+{
+    public record Car(
+        Guid Id, 
+        int Year,
+        string Make,
+        string Model,
+        double Displacement,
+        int Cylinders,
+        int CityFe,
+        int HighwayFe, 
+        int CombinedFe
+        );
+}
