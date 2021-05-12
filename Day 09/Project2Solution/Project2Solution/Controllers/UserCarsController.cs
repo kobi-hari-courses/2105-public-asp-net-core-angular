6ef753843c59225a2dd79fcf143442b8190f01a9
Kobi Hari <kobi2294@yahoo.com>
2021-05-12 18:06:28 +0300

    Project 2, Step 4 completed

---
+++ Day 09/Project2Solution/Project2Solution/Controllers/UserCarsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project2Solution.Entities;
+using Project2Solution.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2Solution.Controllers
+{
+    [Route("api/usercars")]
+    [ApiController]
+    public class UserCarsController : ControllerBase
+    {
+        private IRepositoryService _repo;
+
+        public UserCarsController(IRepositoryService repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("")]
+        public async Task<ActionResult<UserCar>> AddUserCar(UserCar value)
+        {
+            try
+            {
+                var res = await _repo.AddUserCar(value);
+                return Ok(res);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{username}/{carId}")]
+        public async Task<ActionResult> DeleteUserCar(string username, Guid carId)
+        {
+            var value = new UserCar(Username: username, CarId: carId);
+            try
+            {
+                await _repo.RemoveUserCar(value);
+                return Ok();
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<IEnumerable<Car>>> CarsOfUser(string username)
+        {
+            try
+            {
+                var all = await _repo.GetAllCarsOfUser(username);
+                return Ok(all);
+
+            } catch
+            {
+                return NotFound();
+            }
+        }
+
+
+    }
+}
