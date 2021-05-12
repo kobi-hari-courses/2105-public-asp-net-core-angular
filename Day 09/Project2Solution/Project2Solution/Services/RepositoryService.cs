b07637f250b99ba78e1a1339f215fdf634eb35b6
Kobi Hari <kobi2294@yahoo.com>
2021-05-12 16:20:44 +0300

    Step 3 - verification and lazy loading of cars and manufacturers

---
+++ Day 09/Project2Solution/Project2Solution/Services/RepositoryService.cs
@@ -0,0 +1,33 @@
+﻿using Project2Solution.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2Solution.Services
+{
+    public class RepositoryService
+    {
+        private bool _isAllDataLoaded = false;
+        private Dictionary<Guid, Car> _cars;
+        private Dictionary<string, Manufacturer> _manufacturers;
+        private IDataReaderService _dataReader;
+
+        public RepositoryService(IDataReaderService dataReader)
+        {
+            _dataReader = dataReader;
+        }
+
+        private async Task _ensureAllDataLoaded()
+        {
+            if (_isAllDataLoaded) return;
+
+            _isAllDataLoaded = true;
+            _cars = (await _dataReader.GetAllCars())
+                .ToDictionary(c => c.Id);
+
+            _manufacturers = (await _dataReader.GetAllManufacturers())
+                .ToDictionary(m => m.Name);
+        }
+    }
+}
