372d23369d1791b151ca448230d350938702ab5f
Kobi Hari <kobi2294@yahoo.com>
2021-05-12 16:11:24 +0300

    Project 2 - Step 2

---
+++ Day 09/Project2Solution/Project2Solution/Services/DataReaderService.cs
@@ -0,0 +1,36 @@
+﻿using Project2Solution.Entities;
+using Project2Solution.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2Solution.Services
+{
+    public class DataReaderService
+    {
+        // TODO: This should be moved to an external config file
+        private const string _basePath = "Data";
+        private const string _carsFile = "Cars.csv";
+        private const string _mfgFile = "Manufacturers.csv";
+
+        public static Task<IEnumerable<Car>> GetAllCars()
+        {
+            return Task.FromResult(File
+                .ReadAllLines($"{_basePath}/{_carsFile}")
+                .Skip(1)
+                .Where(str => !string.IsNullOrWhiteSpace(str))
+                .Select(str => str.ToCar()));
+        }
+
+        public static Task<IEnumerable<Manufacturer>> GetAllManufacturers()
+        {
+            return Task.FromResult(File
+                .ReadAllLines($"{_basePath}/{_mfgFile}")
+                .Where(str => !string.IsNullOrWhiteSpace(str))
+                .Select(str => str.ToManufacturer()));
+        }
+
+    }
+}
---
+++ Day 09/Project2Solution/Project2Solution/Utils/StringExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Project2Solution.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2Solution.Utils
+{
+    public static class StringExtensions
+    {
+        public static string[] ToColumns(this string source)
+        {
+            return source
+                .Split(',')
+                .Select(s => s.Trim())
+                .ToArray();
+        }
+
+        public static Car ToCar(this string source)
+        {
+            var cols = source.ToColumns();
+
+            return new Car(
+                Id: Guid.NewGuid(),
+                Year: int.Parse(cols[0]),
+                Make: cols[1],
+                Model: cols[2],
+                Displacement: double.Parse(cols[3]),
+                Cylinders: int.Parse(cols[4]),
+                CityFe: int.Parse(cols[5]),
+                HighwayFe: int.Parse(cols[6]),
+                CombinedFe: int.Parse(cols[7])
+            );
+        }
+
+        public static Manufacturer ToManufacturer(this string source)
+        {
+            var cols = source
+                .Split(',')
+                .Select(s => s.Trim())
+                .ToArray();
+
+            return new Manufacturer(
+                Name: cols[0],
+                Country: cols[1],
+                Year: int.Parse(cols[2])
+            );
+        }
+    }
+}
