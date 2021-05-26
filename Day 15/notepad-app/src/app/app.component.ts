44cba3d69e101e11f1494f21a19f7fb792469ddf
kobi2294@yahoo.com <kobi2294@yahoo.com>
2021-05-26 12:00:10 +0300

    notepad completed

---
+++ Day 15/notepad-app/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    // data
+    colors: string[] = ['red', 'green', 'blue', 'brown', 'magenta', 'navy', 'cherry', 'black'];
+    sizes: string[] = ['14px', '18px', '24px', '40px', '80px'];
+    fonts: string[] = ['Arial', 'Verdana', 'Roboto', 'David', 'Segoe UI', 'Courier New'];
+
+    selectedColor: string = this.colors[0];
+    selectedSize: string = this.sizes[0];
+    selectedFont: string = this.fonts[0];
+
+    title: string = 'Amazing Notepad';
+
+
+    // methods
+    selectColor(value: string) {
+        console.log('selecting color ' + value);
+        this.selectedColor = value;
+    }
+
+    selectFont(value: string) {
+        this.selectedFont = value;
+    }
+
+    selectSize(value: string) {
+        this.selectedSize = value;
+    }
+}
